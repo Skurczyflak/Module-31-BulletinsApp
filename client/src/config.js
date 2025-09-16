9001b7e8cb46666632dccf9d710ffe1ee65faa69
Mateusz Matysek <mateusziom1@gmail.com>
2025-09-16 15:06:56 +0200

    Build React app to client/build

--- client/src/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
-
-export const SOCKET_URL = (process.env.NODE_ENV === 'production') ? '' : 'http://localhost:8000';
